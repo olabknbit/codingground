ca8439627da5f847181c11e67d24fb6e3da3636f
olabknbit <al.byczynska@gmail.com>
2015-08-05 00:16:44 +0200

    Created by Coding Ground Client

---
+++ CodeU Session2 task3/BinaryTree.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+
+public class BinaryTree{
+    Node root;
+    
+    public BinaryTree(){
+        this.root = null;
+    }
+    public BinaryTree(Node node){
+        this.root = node;
+    }
+    
+    public Node getRoot(){
+        return this.root;
+    }
+    
+    public BinaryTree add(Node newNode){
+        if(this.root == null){
+            this.root = newNode;
+        }
+        else{
+            Node current = this.root;
+            while((current.getValue() >= newNode.getValue() && current.getLeftChild() != null) || 
+                (current.getValue() < newNode.getValue() && current.getRightChild() != null))
+            {
+                while(current.getValue() >= newNode.getValue() && current.getLeftChild() != null){
+                    current = current.getLeftChild();
+                }
+                while(current.getValue() < newNode.getValue() && current.getRightChild() != null){
+                    current = current.getRightChild();
+                }
+            }
+            if(current.getValue() >= newNode.getValue()){
+                current.setLeftChild(newNode);
+            }
+            else {
+                current.setRightChild(newNode);
+            }
+        }
+        return this;
+        
+    }
+    private void goThroughTree( Map<Integer,String> list, Node current){
+        if(current.getLeftChild() != null) this.goThroughTree(list,current.getLeftChild());
+        list.put(current.getValue(), current.getPayload());
+        //System.out.println(current.getValue()+ " " + current.getPayload());
+        if(current.getRightChild() != null) this.goThroughTree(list, current.getRightChild());
+    }
+    public Map<Integer,String> flatten() {
+        if(this.root == null) {
+                throw new IllegalArgumentException("return value is null at method flatten\n//nothing to flatten;)");  
+            } 
+        Map<Integer,String> flattened = new LinkedHashMap<Integer,String>();
+        this.goThroughTree(flattened, this.root);
+        return flattened;
+    }
+
+}
