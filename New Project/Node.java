3813d6477f2d4f20df46179e7391cfd0799ac0b9
olabknbit <al.byczynska@gmail.com>
2015-08-04 19:23:39 +0200

    Created by Coding Ground Client

---
+++ New Project/Node.java
@@ -0,0 +1,39 @@
+public class Node{
+        Node left;
+        Node right;
+        int value;
+        String payload;
+        public Node(int value){
+            this.left = null;
+            this.right = null;
+            this.value = value;
+            this.payload = "0";
+        }
+        public Node(int value, String payload){
+            this.left = null;
+            this.right = null;
+            this.value = value;
+            this.payload = payload;
+        }
+        /*Getters*/
+        public Node getLeftChild(){
+            return this.left;
+        }
+        public Node getRightChild(){
+            return this.right;
+        }
+        public int getValue(){
+            return this.value;
+        }
+        public String getPayload(){
+            return this.payload;
+        }
+        /*Setters*/
+        public void setLeftChild(Node node){
+            this.left = node;
+        }
+        public void setRightChild(Node node){
+            this.right = node;
+        }
+        
+}
